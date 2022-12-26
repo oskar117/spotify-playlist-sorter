6ffa96d6d52d82541f8bea93f428f55cf1e64910
oskar117 <olek.konieczny@wp.pl>
2022-12-26 14:56:10 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module main
+
+go 1.19
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/nirasan/go-oauth-pkce-code-verifier v0.0.0-20220510032225-4f9f17eaec4c // indirect
+	github.com/zmb3/spotify v1.3.0 // indirect
+	github.com/zmb3/spotify/v2 v2.3.0 // indirect
+	golang.org/x/net v0.3.0 // indirect
+	golang.org/x/oauth2 v0.3.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+// This example demonstrates how to authenticate with Spotify using the authorizati// This example demonstrates how to authenticate with Spotify using the authorization code flow with PKCE.
+// In order to run this example yourself, you'll need to:
+//
+//  1. Register an application at: https://developer.spotify.com/my-applications/
+//     - Use "http://localhost:8080/callback" as the redirect URI
+//  2. Set the SPOTIFY_ID environment variable to the client ID you got in step 1.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	cv "github.com/nirasan/go-oauth-pkce-code-verifier"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2"
+
+	"github.com/zmb3/spotify/v2"
+)
+
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth            = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate), spotifyauth.WithClientID("0673725a49f845f0b2ee585d87c0df67"))
+	ch              = make(chan *spotify.Client)
+	state           = "abc123"
+	codeVerifier, _ = cv.CreateCodeVerifier()
+	codeChallenge   = codeVerifier.CodeChallengeS256()
+	// These should be randomly generated for each request
+	//  More information on generating these can be found here,
+	// https://www.oauth.com/playground/authorization-code-with-pkce.html
+	// codeVerifier  = "w0HfYrKnG8AihqYHA9_XUPTIcqEXQvCQfOF2IitRgmlF43YWJ8dy2b49ZUwVUOR.YnvzVoTBL57BwIhM4ouSa~tdf0eE_OmiMC_ESCcVOe7maSLIk9IOdBhRstAxjCl7"
+	// codeChallenge = "ZhZJzPQXYBMjH8FlGAdYK5AndohLzFfZT-8J7biT7ig"
+)
+
+func main() {
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+	go http.ListenAndServe(":8080", nil)
+
+	url := auth.AuthURL(state,
+		oauth2.SetAuthURLParam("code_challenge_method", "S256"),
+		oauth2.SetAuthURLParam("code_challenge", codeChallenge),
+	)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	// wait for auth to complete
+	client := <-ch
+
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
+	playlistPage, _ := client.GetPlaylistsForUser(context.Background(), user.ID)
+	for _, playlist := range playlistPage.Playlists {
+		if playlist.Owner.ID == user.ID {
+			fmt.Println(playlist.Name)
+		}
+	}
+}
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(r.Context(), state, r,
+		oauth2.SetAuthURLParam("code_verifier", codeVerifier.String()))
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+	// use the token to get an authenticated client
+	client := spotify.New(auth.Client(r.Context(), tok))
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- client
+	close(ch)
+}
