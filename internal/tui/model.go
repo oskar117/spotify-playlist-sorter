e2e61511b699cb5665e923abc4249c2b2d7db13c
oskar117 <olek.konieczny@wp.pl>
2023-01-08 19:47:02 +0100

    Clean up model file

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -24,9 +24,6 @@ func (i ViewArtist) Description() string { return i.Desc }
 func (i ViewArtist) Title() string       { return i.Name }
 
 type model struct {
-	choices     []string         // items on the to-do list
-	cursor      int              // which to-do list item our cursor is pointing at
-	selected    map[int]struct{} // which to-do items are selected
 	artistsList list.Model
 	songGroups  viewport.Model
 	artists     map[string]*sorter.Artist
@@ -38,19 +35,7 @@ func InitialModel(artistNames []list.Item, artists map[string]*sorter.Artist) mo
 	list := list.New(artistNames, delegate, 0, 0)
 	list.Title = "Spotify Playlist Sorter"
 	viewport := viewport.New(0, 0)
-	viewport.SetContent("Test")
-	// list.SetShowHelp(false)
-	// list.SetShowStatusBar(false)
-	// list.SetShowFilter(false)
-	// list.SetFilteringEnabled(false)
 	return model{
-		// Our to-do list is a grocery list
-		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
-
-		// A map which indicates which choices are selected. We're using
-		// the  map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected:    make(map[int]struct{}),
 		artistsList: list,
 		songGroups:  viewport,
 		artists:     artists,
@@ -58,45 +43,11 @@ func InitialModel(artistNames []list.Item, artists map[string]*sorter.Artist) mo
 }
 
 func (m model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
 	return nil
 }
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
 	switch msg := msg.(type) {
-
-	// Is it a key press?
-	// case tea.KeyMsg:
-	//
-	// 	// Cool, what was the actual key pressed?
-	// 	switch msg.String() {
-	//
-	// 	// These keys should exit the program.
-	// 	case "ctrl+c", "q":
-	// 		return m, tea.Quit
-	//
-	// 	// The "up" and "k" keys move the cursor up
-	// 	case "up", "k":
-	// 		if m.cursor > 0 {
-	// 			m.cursor--
-	// 		}
-	//
-	// 	// The "down" and "j" keys move the cursor down
-	// 	case "down", "j":
-	// 		if m.cursor < len(m.choices)-1 {
-	// 			m.cursor++
-	// 		}
-	//
-	// 	// The "enter" key and the spacebar (a literal space) toggle
-	// 	// the selected state for the item that the cursor is pointing at.
-	// 	case "enter", " ":
-	// 		_, ok := m.selected[m.cursor]
-	// 		if ok {
-	// 			delete(m.selected, m.cursor)
-	// 		} else {
-	// 			m.selected[m.cursor] = struct{}{}
-	// 		}
-	// 	}
 	case tea.KeyMsg:
 		if msg.String() == "ctrl+c" {
 			return m, tea.Quit
@@ -111,13 +62,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.songGroups = viewport.New(h/2, v)
 		m.songGroups.SetContent(buildViewport(*m.artists[m.artistsList.SelectedItem().FilterValue()]))
 	}
-	// var cmd tea.Cmd
-	// m.artistsList, cmd = m.artistsList.Update(msg)
-	// cmds = append(cmds, cmd)
 	return m, tea.Batch(cmds...)
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
-	// return m, nil
 }
 
 func buildViewport(choosen sorter.Artist) string {
@@ -132,29 +77,5 @@ func buildViewport(choosen sorter.Artist) string {
 }
 
 func (m model) View() string {
-	// The header
-	s := "What should we buy at the market?\n\n"
-
-	// Iterate over our choices
-	for i, choice := range m.choices {
-
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-	}
-
-	// The footer
-	s += "\nPress q to quit.\n"
 	return lipgloss.JoinHorizontal(lipgloss.Top, m.artistsList.View(), m.songGroups.View())
 }
